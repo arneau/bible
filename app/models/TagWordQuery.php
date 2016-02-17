52c5402bb526f8d5325d8584e62c0a5ff9fc2a17
Arneau van der Horst <github@arneau.me>
2016-02-17 07:02:28 +0200

    Slight refactoring and models

---
+++ app/models/TagWordQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+use Base\TagWordQuery as BaseTagWordQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'tag_word' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class TagWordQuery extends BaseTagWordQuery
+{
+
+}
