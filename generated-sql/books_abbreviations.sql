ae67573c015339107733b8ad6025d48c0dca082f
Arneau van der Horst <github@arneau.me>
2016-07-13 08:40:15 +0200

    Latest DB backup, etc

--- generated-sql/books_abbreviations.sql
+++ generated-sql/books_abbreviations.sql
@@ -1,3 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.6.27, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: defender-app
+-- ------------------------------------------------------
+-- Server version	5.6.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `defender_book_abbreviation`
+--
+
 LOCK TABLES `defender_book_abbreviation` WRITE;
-INSERT INTO `defender_book_abbreviation` VALUES (1,'Gen',1),(2,'Exo',2),(3,'Lev',3),(4,'Num',4),(5,'Deu',5),(6,'Jos',6),(7,'Jdg',7),(8,'Rth',8),(9,'1Sa',9),(10,'2Sa',10),(11,'1Ki',11),(12,'2Ki',12),(13,'1Ch',13),(14,'2Ch',14),(15,'Ezr',15),(16,'Neh',16),(17,'Est',17),(18,'Job',19),(19,'Psalms',20),(19,'Psalm',21),(20,'Pro',22),(21,'Ecc',23),(22,'Sng',24),(23,'Isa',25),(24,'Jer',26),(25,'Lam',27),(26,'Eze',28),(27,'Dan',29),(28,'Hos',30),(29,'Joe',31),(30,'Amo',32),(31,'Oba',33),(32,'Jon',34),(33,'Mic',35),(34,'Nah',36),(35,'Hab',37),(36,'Zep',38),(37,'Hag',39),(38,'Zec',40),(39,'Mal',41),(40,'Mat',42),(41,'Mar',43),(42,'Luk',44),(43,'Jhn',45),(44,'Act',46),(45,'Rom',47),(46,'1Co',48),(47,'2Co',49),(48,'Gal',50),(49,'Eph',51),(50,'Phl',52),(51,'Col',53),(52,'1Th',54),(53,'2Th',55),(54,'1Ti',56),(55,'2Ti',57),(56,'Tit',58),(57,'Phm',59),(58,'Heb',60),(59,'Jas',61),(60,'1Pe',62),(61,'2Pe',63),(62,'1Jo',64),(63,'2Jo',65),(64,'3Jo',66),(65,'Jde',67),(66,'Rev',68);
+/*!40000 ALTER TABLE `defender_book_abbreviation` DISABLE KEYS */;
+INSERT INTO `defender_book_abbreviation` VALUES (1,'Gen',1),(2,'Exo',2),(3,'Lev',3),(4,'Num',4),(5,'Deu',5),(6,'Jos',6),(7,'Jdg',7),(8,'Rth',8),(9,'1Sa',9),(10,'2Sa',10),(11,'1Ki',11),(12,'2Ki',12),(13,'1Ch',13),(14,'2Ch',14),(15,'Ezr',15),(16,'Neh',16),(17,'Est',17),(18,'Job',19),(19,'Psalms',20),(19,'Psalm',21),(20,'Pro',22),(21,'Ecc',23),(22,'Sng',24),(23,'Isa',25),(24,'Jer',26),(25,'Lam',27),(26,'Eze',28),(27,'Dan',29),(28,'Hos',30),(29,'Joe',31),(30,'Amo',32),(31,'Oba',33),(32,'Jon',34),(33,'Mic',35),(34,'Nah',36),(35,'Hab',37),(36,'Zep',38),(37,'Hag',39),(38,'Zec',40),(39,'Mal',41),(40,'Mat',42),(41,'Mar',43),(42,'Luk',44),(43,'Jhn',45),(44,'Act',46),(45,'Rom',47),(46,'1Co',48),(47,'2Co',49),(48,'Gal',50),(49,'Eph',51),(50,'Phl',52),(51,'Col',53),(52,'1Th',54),(53,'2Th',55),(54,'1Ti',56),(55,'2Ti',57),(56,'Tit',58),(57,'Phm',59),(58,'Heb',60),(59,'Jas',61),(60,'1Pe',62),(61,'2Pe',63),(62,'1Jo',64),(63,'2Jo',65),(64,'3Jo',66),(65,'Jde',67),(66,'Rev',68),(19,'Psa',69),(20,'Prov',70),(5,'Deut',71);
+/*!40000 ALTER TABLE `defender_book_abbreviation` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-13  8:39:52
